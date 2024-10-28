c553321d0dbc4ff09dc1c0af42265809628bbec1
iftykhar alam <s.m.ifty49@gmail.com>
2024-10-28 15:49:36 +0600

    dreams table

--- database/migrations/2024_10_28_094901_create_dreams_table.php
+++ database/migrations/2024_10_28_094901_create_dreams_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-
-            $table->boolean('is_admin')->default(false);
+        Schema::create('dreams', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -22,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-           $table->dropColumn('is_admin');
-        });
+        Schema::dropIfExists('dreams');
     }
 };
