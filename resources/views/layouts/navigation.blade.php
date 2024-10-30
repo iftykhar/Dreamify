0a6704286af53c55237abf9b85f3b0d5bc470d10
iftykhar alam <s.m.ifty49@gmail.com>
2024-10-30 11:31:53 +0600

    controller and view setting initiated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('dreams.index')" :active="request()->routeIs('dreams.index')">
+                        Dreams
+                    </x-nav-link>
+                    <x-nav-link :href="route('dreams.create')" :active="request()->routeIs('dreams.create')">
+                        Post A Dreams
                     </x-nav-link>
                 </div>
             </div>
@@ -67,8 +70,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('dreams.index')" :active="request()->routeIs('dreams.index')">
+                    Dreams
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dreams.create')" :active="request()->routeIs('dreams.create')">
+                    Post A Dreams
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\DreamController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/admin/dreams',[DreamController::class,'index']);
+// Route::get('/admin/dreams/{dream}',[DreamController::class,'show']);
+// Route::get('/admin/dreams/create',[DreamController::class,'create']);
+// Route::post('/admin/dreams/store',[DreamController::class,'store']);
+// Route::get('/admin/dreams/{dream}/edit',[DreamController::class,'edit']);
+// Route::put('/admin/dreams/{dream}/update',action: [DreamController::class,'update']);
+// Route::delete('/admin/dreams/{dream}/destroy',action: [DreamController::class,'destroy']);
+
+
+Route::resource('admin/dreams', DreamController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
