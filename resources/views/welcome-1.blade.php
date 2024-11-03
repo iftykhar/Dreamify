0ff51f1b1c2051bebfedd4ad060011be9cc6375d
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-03 23:05:29 +0600

    adding a new view file

--- resources/views/welcome-1.blade.php
+++ resources/views/welcome-1.blade.php
@@ -30,7 +30,7 @@
                 </div>
 
 
-                
+
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <a class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/admin/dreams">
